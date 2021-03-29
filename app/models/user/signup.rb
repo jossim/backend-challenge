9be9f4b399a02766d38368630b84a47aa0fbe260
Joseph Simmons <josephdsimmons@gmail.com>
2021-03-29 08:02:09 -0500

    Adds some documentation to User::Signup

--- app/models/user/signup.rb
+++ app/models/user/signup.rb
@@ -1,3 +1,5 @@
+# This model is used to create a user when you want callbacks to be used to
+# shortened their URL and, fetch and save their website
 class User::Signup < User
   after_create :shorten_url, :set_website_content
 
